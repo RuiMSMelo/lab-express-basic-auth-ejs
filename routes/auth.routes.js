cc9366a7e3f9af485be5fc10cffbf83a049d498a
RuiMSMelo <ruimsmelo@gmail.com>
2023-04-25 17:17:27 +0100

    iteration 2 done

---
+++ routes/auth.routes.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const User = require('../models/User.model')
+const router = express.Router()
+const bcryptjs = require('bcryptjs')
+const saltRounds = 13
+
+router.get('/signup', (req, res, next) => {
+    res.render('auth/signup')
+})
+
+router.post('/signup', async (req, res, next) => {
+    try {
+        const potentialUser = await User.findOne({username: req.body.username})
+        console.log(potentialUser)
+
+        if(!potentialUser){
+            const salt = bcryptjs.genSaltSync(saltRounds)
+
+            const passwordHash = bcryptjs.hashSync(req.body.password, salt)
+
+            const newUser = await User.create({username: req.body.username, passwordHash})
+
+            res.redirect('/auth/login')  
+        } else {
+            res.render('auth/signup', {errorMessage: 'Username already in use'})
+        }
+
+    } catch (error) {
+        console.log(error)
+    }
+
+})
+
+router.get('/login', (req, res, next) => {
+    res.render('auth/login')
+})
+
+router.post('/login', async (req, res, next) => {
+    try {
+        const user = await User.findOne({username: req.body.username})
+        console.log(user)
+        if (!!user){
+            if (bcryptjs.compareSync(req.body.password, user.passwordHash)) {
+                res.send(user)
+            } else {
+                res.render('auth/login', {errorMessage: 'Password not correct'})
+            }
+        } else {
+            res.render('auth/login', {errorMessage: 'Username does not exist'})
+        }
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+module.exports = router
